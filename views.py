c9aad6e9a79747aaa2d7f9d7188e36a1d0f05672
yadhuvir <yadhuvir.v@gmail.com>
2024-08-11 11:44:42 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def shop(request):
+    return render(request, 'shop.html')
+    
